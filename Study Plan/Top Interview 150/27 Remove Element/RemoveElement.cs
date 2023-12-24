9959265f8559590d4356b7866b4e409cbe1efc01
Nano Taboada <87288+nanotaboada@users.noreply.github.com>
2023-12-24 19:46:16 -0300

    refactor: update 1 solution for the Top Interview 150 study plan

--- Study Plan/Top Interview 150/27 Remove Element/RemoveElement.cs
+++ Study Plan/Top Interview 150/27 Remove Element/RemoveElement.cs
@@ -7,16 +7,30 @@
 /*  
     Solution
     --------------------------------------------------------------------------------
-    - Runtime 141 ms (Beats 5.99% of users with C#)
-    - Memory 42.84 MB (Beats 40.50% of users with C#)
+    - Runtime 101 ms (Beats 97.62% of users with C#)
+    - Memory 45.78 MB (Beats 5.42% of users with C#)
 */
 public class Solution
 {
     public int RemoveElement(int[] nums, int val)
     {
-        var filtered = nums.Where(value => value != val).ToArray();
-        Array.Copy(filtered, nums, filtered.Length);
+        if (nums.Length == 0) return 0;
 
-        return filtered.Length;
+        // Consider the number of elements in nums which are not equal to val
+        // be k
+        var k = 0;
+
+        for (var index = 0; index < nums.Length; index++)
+        {
+            if (nums[index] != val)
+            {
+                // Swap the non-val element to the front
+                nums[k] = nums[index];
+                // Increment the number of non-val elements
+                k++;
+            }
+        }
+
+        return k;
     }
 }
