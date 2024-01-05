7e376a7220a63ad6dee083196cdab27a6a3f12c9
Nano Taboada <87288+nanotaboada@users.noreply.github.com>
2024-01-04 22:38:45 -0300

    feat: add 1 solution in C# and Java for the LeetCode 75 study plan

---
+++ Study Plan/LeetCode 75/238 Product of Array Except Self/ProductOfArrayExceptSelf.java
@@ -0,0 +1,94 @@
+/*
+    Problem
+    --------------------------------------------------------------------------------
+    238. Product of Array Except Self
+*/
+
+/*
+    Solution
+    --------------------------------------------------------------------------------
+
+    - Cycle the collection left-to-right (from the second element to the last),
+    calculating the poduct of the elements to its left.
+
+            ---------------->  leftToRight  
+    nums    [       1       ,       2       ,       3       ,       4       ]
+                                    1
+
+            --------------- * -------------->  leftToRight
+    nums    [       1       ,       2       ,       3       ,       4       ]
+                                                  1 * 2
+
+            --------------- * ------------- * -------------->  leftToRight
+    nums    [       1       ,       2       ,       3       ,       4       ]
+                                                                1 * 2 * 3
+
+            -----------------------------------------------------------------
+
+    left    [               ,       1       ,       2     ,          6      ]
+
+    - Cycle the collection right-to-left (from the second to last to the first),
+    calculating the poduct of the elements to its right.
+
+                                        rightToLeft  <---------------
+    [       1       ,       2       ,       3       ,       4       ]   nums
+                                            4
+
+                       rightToLeft  <-------------- * ---------------
+    [       1       ,       2       ,       3       ,       4       ]   nums
+                          3 * 4
+
+       rightToLeft  <-------------- * ------------- * ---------------
+    [       1       ,       2       ,       3       ,       4       ]   nums
+        2 * 3 * 4
+
+    -----------------------------------------------------------------
+
+    [       24      ,       12      ,       4       ,               ]   right
+
+    - Finally, cycle the results collection (output), calculating
+    left * right.
+
+    left    [               ,       1       ,       2       ,       6      ]
+    right   [       24      ,      12       ,       4       ,              ]
+            ----------------------------------------------------------------
+            [       24      ,    1 * 12     ,     2 * 4     ,       6      ]
+
+            ----------------------------------------------------------------
+
+    output  [       24      ,        12     ,       8       ,       6      ]
+
+    https://leetcode.com/problems/product-of-array-except-self/solutions/4508578/java-time-and-space-complexity-0-n-runtime-1ms-beats-100-step-by-step-explanation/
+
+    - Runtime 1 ms (Beats 100.00% of users with Java)
+    - Memory 54.81 MB (Beats 5.82% of users with Java)
+*/
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] output = new int[nums.length];
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+        // A new array is initialized with zeros, so for the product to make
+        // sense, we need to set the first element of left and the last element
+        // of right to 1, otherwise it will cause n * 0 = 0
+        left[0] = 1;
+        right[nums.length - 1] = 1;
+        // Cycle the collection left-to-right (from the second element to the last),
+        for (int leftToRight = 1; leftToRight < nums.length; leftToRight++) {
+            // calculating the poduct of the elements to its left.
+            left[leftToRight] = left[leftToRight - 1] * nums[leftToRight - 1];
+        }
+        // Cycle the collection right-to-left (from the second to last to the first),
+        for (int rightToLeft = nums.length - 2; rightToLeft >= 0; rightToLeft--) {
+            // calculating the poduct of the elements to its right.
+            right[rightToLeft] = right[rightToLeft + 1] * nums[rightToLeft + 1];
+        }
+        // Finally, cycle the results collection (output),
+        for (int index = 0; index < output.length; index++) {
+            // calculating left * right
+            output[index] = left[index] * right[index];
+        }
+        return output;
+    }
+}
