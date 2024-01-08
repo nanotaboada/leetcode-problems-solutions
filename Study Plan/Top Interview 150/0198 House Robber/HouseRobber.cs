0ff8d9c042663b67b32ecbc59d53acb8f38cd443
Nano Taboada <87288+nanotaboada@users.noreply.github.com>
2024-01-08 13:44:47 -0300

    chore: update 1 solution in C# for the Top Interview 150 study plan

--- Study Plan/Top Interview 150/0198 House Robber/HouseRobber.cs
+++ Study Plan/Top Interview 150/0198 House Robber/HouseRobber.cs
@@ -115,11 +115,10 @@ public class Solution
 {
     public int Rob(int[] nums)
     {
-        if (nums.Length < 2) return nums[0];
-        if (nums.Length == 2) return Math.Max(nums[0], nums[1]);
-
         var street = nums.Length;
-        
+        if (street < 2) return nums[0];
+        if (street == 2) return Math.Max(nums[0], nums[1]);
+
         var money = new int[street];
         money[0] = nums[0];
         money[1] = Math.Max(nums[0], nums[1]);
